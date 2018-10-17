54be71f68ced75e18aa1f9bd9336edfa9fe042f8
Denise Yu <dyu@pivotal.io>
2018-10-17 16:55:25 -0400

    Chruby looks for ruby v2.4.4
    Signed-off-by: Rebecca Putinski <rputinski@pivotal.io>

--- shared/tasks/teardown.sh
+++ shared/tasks/teardown.sh
@@ -4,7 +4,7 @@ set -e
 
 source pipelines/shared/utils.sh
 source /etc/profile.d/chruby.sh
-chruby 2.1.7
+chruby 2.4.4
 
 if [ ! -e director-state/director-state.json ]; then
   echo "director-state.json does not exist, skipping..."
