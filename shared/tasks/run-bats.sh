09408a33b7f06f6bc358c04e56856f8a4e4558de
Joseph Palermo <jpalermo@pivotal.io>
2023-12-07 16:12:19 -0800

    Update bats config generation to account for changes in bats

--- shared/tasks/run-bats.sh
+++ shared/tasks/run-bats.sh
@@ -26,11 +26,6 @@ export BAT_STEMCELL=$(realpath stemcell/*.tgz)
 export BAT_DEPLOYMENT_SPEC=$(realpath bats-config/bats-config.yml)
 export BAT_BOSH_CLI=$(which bosh)
 
-ssh_key_path=/tmp/bat_private_key
-echo "$BAT_PRIVATE_KEY" > $ssh_key_path
-chmod 600 $ssh_key_path
-export BOSH_GW_PRIVATE_KEY=$ssh_key_path
-
 pushd bats
   bundle install
   bundle exec rspec spec $BAT_RSPEC_FLAGS
