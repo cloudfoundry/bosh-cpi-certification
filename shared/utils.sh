302f0940d27cadb1cb153f769a3d1c1e541e4378
Nitin Ravindran <nitin.ravindran@broadcom.com>
2024-10-08 11:08:26 -0700

    Remove bosh-cli as inputs to concourse tasks
    The cli is already included in the bosh/integration docker image
    
    Signed-off-by: Joseph Palermo <joseph.palermo@broadcom.com>
    Co-authored-by: Joseph Palermo <joseph.palermo@broadcom.com>

--- shared/utils.sh
+++ shared/utils.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-# Oportunistically configure bosh for use
-configure_bosh_cli() {
-  local bosh_input="$(realpath bosh-cli/*bosh-cli-* 2>/dev/null || true)"
-  if [[ -n "${bosh_input}" ]]; then
-    export bosh_cli="/usr/local/bin/bosh"
-    cp "${bosh_input}" "${bosh_cli}"
-    chmod +x "${bosh_cli}"
-  fi
-}
-configure_bosh_cli
-
 state_path() { bosh int director-state/director.yml --path="$1" ; }
 creds_path() { bosh int director-state/creds.yml --path="$1" ; }
 
